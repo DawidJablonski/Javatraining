c150cc0463d87e00b58f77a02b30d4eed723e6f5
dawid_test <dawid_test@mbp-dawid-test.home>
2020-02-16 19:43:56 +0100

    My first commit.

---
+++ Test/src/com/dawid/test/Main.java
@@ -0,0 +1,15 @@
+package com.dawid.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome Dawid");// TODO Auto-generated method stub
+        int a=10; 
+        int z=100;
+        System.out.println(z);
+        System.out.println(z+a);
+        System.out.println(z-a); // Podstawowa metoda wyswietlania z przejsciem do nowej linii
+        System.out.print("Dawid");// podstawowa metoda wyswietlanie bez przejscia do nowej linii 
+	}
+
+}
